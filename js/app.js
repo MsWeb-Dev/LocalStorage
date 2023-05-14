543809034ffcff5f03f3888bef312578e7f63c06
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-14 17:18:25 +0500

    Json farmatdagilarni stringga stringni jsonga otkazish

--- js/app.js
+++ js/app.js
@@ -12,4 +12,15 @@
 
 // localStorage.clear()
 
+const ismlar = ["Diyor","Javohir","Alisher"]
 
+localStorage.setItem("nameArray",JSON.stringify(ismlar))
+
+
+console.log(ismlar)
+
+// malumotlar string tipida boryapti JSON.stringifydan foydalanib json formatga otkazamiz
+
+const arrName = JSON.parse(localStorage.getItem("nameArray"))
+//JSON parse metodi orqali string turidagi malumotni JSON ga aylantirish mumkin
+console.log(arrName)
