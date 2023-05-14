45d0a5b81f864f93938c014a4cae1dbc04dcfd08
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-14 17:06:12 +0500

    Local strogega misol yozildi

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 // get iteem 
 // set item
 //clear()
+
+// const ism = "Shaxnoza"
+
+// localStorage.setItem("name",ism)
+
+// const getName = localStorage.getItem("name")
+
+// console.log(getName)
+
+// localStorage.clear()
+
+
