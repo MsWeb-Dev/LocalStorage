658f46c2baa3a36ada3dd43fdbc30d34f28d6aab
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-15 14:06:14 +0500

    finish

--- uyga vazifa/app.js
+++ uyga vazifa/app.js
@@ -1,3 +1,16 @@
-const input = document.querySelector("input").value
-localStorage.getItem("ism",input)
-console.log(input)
+$(document).ready(function(){
+
+   
+    $(".btn").click(()=>{    
+   let get = $(".ism").val()
+   let set = $(".ismm").val()
+   localStorage.setItem( get, set)
+
+
+
+
+     
+    }) 
+ 
+ 
+ })
